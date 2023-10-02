6890582cd6e2b9b920a3e29c7ff0cb4189ac147f
tah01 <tahirkadgaonkar01@gmail.com>
2023-10-02 19:23:29 +0530

    logic of event handling implemented (data-handling)

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,17 +1,32 @@
+import { useState } from "react";
+
+const d = "duration";
 const UserInput = () => {
+  const initialUserInput = {
+    "current-savings": 10000,
+    "yearly-contribution": 1200,
+    "expected-return": 7,
+    [d]: 10,
+  };
+  //setting state
+  const [userInput, setUserInput] = useState(initialUserInput);
+
+  //form submit
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log("submitHandler");
   };
 
+  //reseting values
   const resetHandler = (e) => {
-    console.log("resetHandler");
+    setUserInput(initialUserInput);
   };
 
+  //input feild updating
   const inputChangeHandler = (identifier, value) => {
-    console.log(identifier, value);
+    setUserInput((prev) => ({ ...prev, [identifier]: value }));
   };
 
+  console.log(userInput);
   return (
     <form onSubmit={submitHandler} className="form">
       <div className="input-group">
@@ -21,6 +36,7 @@ const UserInput = () => {
             onChange={(e) =>
               inputChangeHandler("current-savings", e.target.value)
             }
+            value={userInput["current-savings"]}
             type="number"
             id="current-savings"
           />
@@ -31,6 +47,7 @@ const UserInput = () => {
             onChange={(e) =>
               inputChangeHandler("yearly-contribution", e.target.value)
             }
+            value={userInput["yearly-contribution"]}
             type="number"
             id="yearly-contribution"
           />
@@ -45,6 +62,7 @@ const UserInput = () => {
             onChange={(e) =>
               inputChangeHandler("expected-return", e.target.value)
             }
+            value={userInput["expected-return"]}
             type="number"
             id="expected-return"
           />
@@ -53,6 +71,7 @@ const UserInput = () => {
           <label htmlFor="duration">Investment Duration (years)</label>
           <input
             onChange={(e) => inputChangeHandler("duration", e.target.value)}
+            value={userInput["duration"]}
             type="number"
             id="duration"
           />
